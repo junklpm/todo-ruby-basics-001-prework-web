56a787dc0290e1c68747e50c61764cfe174ab08b
junklpm <lance.p.miller@gmail.com>
2016-05-28 10:19:18 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ num1 = 12
+ num2 = 6
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
 end
 
-def greeting
-
-end
-
-def return_a_value
+def greeting (greeting, name)
 
 end
 
-def last_evaluated_value
-
+def return_a_value (phrase = "Nice")
+puts phrase
+return phrase
 end
 
-def pizza_party
+def last_evaluated_value (phrase = "expert")
+puts phrase
+return phrase
+end
 
+def pizza_party (phrase = "cheese")
+puts phrase
+return phrase
 end
